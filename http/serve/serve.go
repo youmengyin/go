8c07f6074d8aa170dc23781663f380074004788d
mcwmengxi <1395568275@qq.com>
2022-12-23 19:23:27 +0800

    http: update

--- http/serve/serve.go
+++ http/serve/serve.go
@@ -3,12 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
+
+type timeHandle struct {
+	format string
+}
+
+func (th *timeHandle) ServeHttp(res http.ResponseWriter, req *http.Request) {
+	currentTime := time.Now().Format(th.format)
+	res.Write([]byte("current time: " + currentTime))
+}
 
 func main() {
 	// ServeMux 和 Handler
 	mux := http.NewServeMux()
 	mux.HandleFunc("/test", handle)
+	th := &timeHandle{format: time.RFC1123}
+	mux.Handle("/time", th)
 	log.Println("Listening...")
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
